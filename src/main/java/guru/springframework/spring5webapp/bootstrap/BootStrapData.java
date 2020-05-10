29e860cc4b3371dff95be6b0065cc4246590ce6d
Josue Villasante <javillasantet@gmail.com>
2020-05-09 23:41:59 -0500

    Added bootstrapdata

---
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -0,0 +1,45 @@
+package guru.springframework.spring5webapp.bootstrap;
+
+import guru.springframework.spring5webapp.domain.Author;
+import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import guru.springframework.spring5webapp.repositories.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootStrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BootStrapData(AuthorRepository authorRepository,
+                         BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Evans", "Eric");
+        Book book = new Book("Domain drive design", "123");
+        eric.getBooks().add(book);
+        book.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(book);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE  Development  without EJB", "123123");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started bootstrap");
+        System.out.println("Number of books: " + bookRepository.count());
+    }
+}
--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5webapp.domain;
 
 import javax.persistence.*;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -14,15 +15,14 @@ public class Author {
     private String firstName;
 
     @ManyToMany(mappedBy = "authors")
-    private Set<Book> books;
+    private Set<Book> books = new HashSet<>();
 
     public Author() {
     }
 
-    public Author(String lastName, String firstName, Set<Book> books) {
+    public Author(String lastName, String firstName) {
         this.lastName = lastName;
         this.firstName = firstName;
-        this.books = books;
     }
 
     public Long getId() {
